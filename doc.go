e206fbe3885eabc3683f34b10876fa0da3720be9
Yahia Sweid <yahia.sweid@gmail.com>
2025-11-25 15:07:29 +0800

    Release doc.go

---
+++ doc.go
@@ -0,0 +1,78 @@
+// # Query APIs
+//
+// - [Client.ListClientAccounts]
+//
+// - [Client.GetClientProfile]
+//
+// - [Client.GetFund]
+//
+// - [Client.GetRequestByDuitNowEndToEndID]
+//
+// - [Client.GetClientAccountAllocationPerformance]
+//
+// - [Client.GetClientAccountStatement]
+//
+// - [Client.GetClientAccountRequestConfirmation]
+//
+// - [Client.GetClientReferral]
+//
+// - [Client.GetClientAccountRequestPolicy]
+//
+// - [Client.ListFundsForSubscription]
+//
+// - [Client.ListClientAccountBalance]
+//
+// - [Client.ListClientAccountRequests]
+//
+// - [Client.ListClientBankAccounts]
+//
+// - [Client.ListDisplayCurrencies]
+//
+// - [Client.ListClientSuitabilityAssessments]
+//
+// - [Client.ListDuitNowBanks]
+//
+// - [Client.ListInvestConsents]
+//
+// - [Client.ListBanks]
+//
+// - [Client.ListClientAccountMandateRequests]
+//
+// - [Client.ListClientPromos]
+//
+// - [Client.ListClientAccountPerformance]
+//
+// - [Client.ListPaymentMethods]
+//
+// - [Client.GetVoucher]
+//
+// - [Client.GetPreviewInvest]
+//
+// - [Client.GetProjectedFundPrice]
+//
+// # Command APIs
+//
+// - [Client.CreateInvestmentRequest]
+//
+// - [Client.CreateRedemptionRequest]
+//
+// - [Client.CreateSwitchRequest]
+//
+// - [Client.CreateRequestCancellation]
+//
+// - [Client.CreateWithdrawalRequest]
+//
+// - [Client.CreateDepositRequest]
+//
+// - [Client.CreateSuitabilityAssessment]
+//
+// - [Client.CreateClientBankAccount]
+//
+// - [Client.UpdateDisplayCurrency]
+//
+// - [Client.UpdateAccountName]
+//
+// - [Client.CreateDuitnowPayment]
+//
+// - [Client.UpdateClientProfile]
+package wallet
