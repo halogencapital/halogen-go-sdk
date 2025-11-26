e109dca052532799c1fdd2aaf91a4e80e98e120a
Yahia Sweid <yahia.sweid@gmail.com>
2025-11-26 10:00:32 +0800

    Remove swagger HTML loader

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	endpoint  string = "https://external-api.wallet.halogen.my"
-	version   string = "0.0.5"
+	version   string = "0.0.6"
 	userAgent string = "wallet/" + version + " lang/go"
 )
 
